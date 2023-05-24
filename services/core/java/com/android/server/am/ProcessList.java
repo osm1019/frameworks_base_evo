8158045a50466e05e41fbee6b1f986844e033935
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-24 22:43:31 +0800

    ProcessList: Catch Security exception when starting a frozen package
    05-24 21:46:13.689  1418  1461 E ActivityManager: Failure starting process com.android.launcher3
    05-24 21:46:13.689  1418  1461 E ActivityManager: java.lang.SecurityException: Package com.android.launcher3 is currently frozen!
    05-24 21:46:13.689  1418  1461 E ActivityManager: 	at com.android.server.pm.PackageManagerService.checkPackageStartable(PackageManagerService.java:4513)
    05-24 21:46:13.689  1418  1461 E ActivityManager: 	at com.android.server.pm.PackageManagerService$IPackageManagerImpl.checkPackageStartable(PackageManagerService.java:4595)
    
    Change-Id: I2be5ebb45dce51938a08c5415d27e811847c0531
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- services/core/java/com/android/server/am/ProcessList.java
+++ services/core/java/com/android/server/am/ProcessList.java
@@ -1880,6 +1880,7 @@ public final class ProcessList {
                 AppGlobals.getPackageManager().checkPackageStartable(app.info.packageName, userId);
             } catch (RemoteException e) {
                 throw e.rethrowAsRuntimeException();
+            } catch (SecurityException e) {
             }
 
             int uid = app.uid;
