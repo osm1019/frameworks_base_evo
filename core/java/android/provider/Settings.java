909827b71bf8855b782582a79d6c6a381a648803
Lokesh Chamane <lokesh.chamane@pac-rom.com>
2024-08-03 10:44:21 +0530

    SystemUI: Battery bar gradient color [1/2]
    - fix low gradient color is persistent red / not changing.
    
    Change-Id: I1ef5b60908ceaf205e6dbb83899fac9604cf6bf5
    Co-authored-by: AnkitGourav <Ankitgourav035@gmail.com>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7033,8 +7033,8 @@ public final class Settings {
         /**
          * @hide
          */
-        public static final String STATUSBAR_BATTERY_BAR_BATTERY_LOW_COLOR =
-                "statusbar_battery_bar_battery_low_color";
+        public static final String STATUSBAR_BATTERY_BAR_BATTERY_LOW_COLOR_WARNING =
+                "statusbar_battery_bar_battery_low_color_warning";
 
         /**
          * @hide
@@ -7054,7 +7054,22 @@ public final class Settings {
                 "statusbar_battery_bar_blend_color_reverse";
 
         /**
-        * Keys we no longer back up under the current schema, but want to continue to
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_HIGH_COLOR = "statusbar_battery_bar_high_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_LOW_COLOR = "statusbar_battery_bar_low_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_USE_GRADIENT_COLOR = "statusbar_battery_bar_use_gradient_color";
+
+        /**
+         * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
          * All settings in {@link LEGACY_RESTORE_SETTINGS} array *must* have a non-null validator,
