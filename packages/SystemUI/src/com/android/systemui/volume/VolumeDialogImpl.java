8ec2a3270c2eb5fc21334850113bcaefe44e51d1
minaripenguin <minaripenguin@users.noreply.github.com>
2025-01-03 10:23:37 +0800

    SystemUI: Move volume slider haptics changes to VolumeUtils
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -948,8 +948,6 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
     }
 
     private void addSliderHapticsToRow(VolumeRow row) {
-        row.createPlugin(row.slider, mVibratorHelper, mMSDLPlayer, mSystemClock);
-        HapticSliderViewBinder.bind(row.slider, row.mHapticPlugin);
     }
 
     @VisibleForTesting void addSliderHapticsToRows() {
@@ -2133,6 +2131,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
             return;  // don't update if user is sliding
         }
         final int progress = row.slider.getProgress();
+        mVolumeUtils.performVolumeHaptics(mShowing, progress, row.ss.levelMax);
         final int level = getVolumeFromProgress(row.ss, row.slider, progress);
         final boolean rowVisible = row.view.getVisibility() == VISIBLE;
         final boolean inGracePeriod = (SystemClock.uptimeMillis() - row.userAttempt)
@@ -2643,9 +2642,7 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                             userLevel);
                 }
             }
-            int vibrateIntensity = Settings.System.getInt(mContext.getContentResolver(),
-                        Settings.System.VOLUME_SLIDER_HAPTICS_INTENSITY, 1);
-            VibrationUtils.triggerVibration(mContext, vibrateIntensity);
+            mVolumeUtils.performVolumeHaptics(mShowing, mRow.slider.getProgress(), mRow.ss.levelMax);
         }
 
         @Override
