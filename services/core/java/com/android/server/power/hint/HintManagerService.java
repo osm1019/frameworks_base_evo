084e484d665d0b229e4d8941a4bfab10774950b4
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-11-13 20:32:07 +0800

    [bugfix] fixing powerhal soft reboot
    Change-Id: I32df30b4eeba4f56786e568f706eea23cfe23de8
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- services/core/java/com/android/server/power/hint/HintManagerService.java
+++ services/core/java/com/android/server/power/hint/HintManagerService.java
@@ -1089,10 +1089,11 @@ public final class HintManagerService extends SystemService {
             if (mConfig != null) {
                 try {
                     mPowerHal.closeSessionChannel(mTgid, mUid);
-                } catch (RemoteException e) {
-                    throw new IllegalStateException("Failed to close session channel!", e);
+                } catch (Exception e) {
+                    Slog.w(TAG, "Session channel already dead for uid " + mUid);
+                } finally {
+                    mConfig = null;
                 }
-                mConfig = null;
             }
         }
 
