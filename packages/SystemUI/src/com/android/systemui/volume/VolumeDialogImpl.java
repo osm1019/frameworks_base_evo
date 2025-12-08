ProjectDiablo/frameworks_base

SystemUI: Implement intensity-based volume slider haptics
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -155,6 +155,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
+import com.android.internal.util.android.VibrationUtils;
+
 /**
  * Visual presentation of the volume dialog.
  *
@@ -2999,6 +3001,9 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                             userLevel);
                 }
             }
+            int vibrateIntensity = Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.VOLUME_SLIDER_HAPTICS_INTENSITY, 1);
+            VibrationUtils.triggerVibration(mContext, vibrateIntensity);
         }
 
         @Override

osm1019/frameworks_base_evo

Change-Id: Ia67f37dc568c5ff3e112aca6a6f318f7dbdc3168
Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -115,6 +115,7 @@ import com.android.internal.annotations.GuardedBy;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.internal.graphics.drawable.BackgroundBlurDrawable;
 import com.android.internal.jank.InteractionJankMonitor;
+import com.android.internal.util.evolution.VibrationUtils;
 import com.android.internal.view.RotationPolicy;
 import com.android.settingslib.Utils;
 import com.android.systemui.Dumpable;
@@ -2637,6 +2638,9 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
                             userLevel);
                 }
             }
+            int vibrateIntensity = Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.VOLUME_SLIDER_HAPTICS_INTENSITY, 1);
+            VibrationUtils.triggerVibration(mContext, vibrateIntensity);
         }
 
         @Override