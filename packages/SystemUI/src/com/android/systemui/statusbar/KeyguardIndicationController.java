ed7d151ac481602d72de8023de818a97dcff13d3
Oscar <osm1019@gmail.com>
2026-07-30 15:57:21 -0700

    SystemUI: preserve adapter values for charging stats
    Use the current and voltage supplied by BatteryService so VOOC devices
    calculate lockscreen wattage from adapter-side values instead of battery
    cell voltage.

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -199,7 +199,6 @@ public class KeyguardIndicationController {
     protected final @Background DelayableExecutor mBackgroundExecutor;
     private final LockPatternUtils mLockPatternUtils;
     private final FalsingManager mFalsingManager;
-    private final BatteryManager mBatteryManager;
     private final KeyguardBypassController mKeyguardBypassController;
     private final AccessibilityManager mAccessibilityManager;
     private final Handler mHandler;
@@ -410,7 +409,6 @@ public class KeyguardIndicationController {
         mLockPatternUtils = lockPatternUtils;
         mAuthController = authController;
         mFalsingManager = falsingManager;
-        mBatteryManager = mContext.getSystemService(BatteryManager.class);
         mKeyguardBypassController = keyguardBypassController;
         mAccessibilityManager = accessibilityManager;
         mScreenLifecycle = screenLifecycle;
@@ -1698,14 +1696,9 @@ public class KeyguardIndicationController {
             mPowerPluggedInDock = status.isPluggedInDock() && isChargingOrFull;
             mPowerPluggedIn = isPowerPluggedIn(status, isChargingOrFull);
             mPowerCharged = status.isCharged();
-            final Intent batteryIntent = mContext.registerReceiver(
-                    null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
-            mChargingCurrent = getRealtimeChargingCurrent(
-                    isChargingOrFull, status.maxChargingCurrent);
-            mChargingVoltage = getRealtimeChargingVoltage(
-                    batteryIntent, status.maxChargingVoltage);
-            mChargingWattage = getRealtimeChargingWattage(
-                    mChargingCurrent, mChargingVoltage, status.maxChargingWattage);
+            mChargingCurrent = status.maxChargingCurrent;
+            mChargingVoltage = status.maxChargingVoltage;
+            mChargingWattage = status.maxChargingWattage;
             mChargingSpeed = status.getChargingSpeed(mContext);
             mChargingStatus = status.chargingStatus;
             mBatteryLevel = status.level;
@@ -1750,62 +1743,6 @@ public class KeyguardIndicationController {
             if (ScrimUtils.get().isKeyguardShowing()) {
                 updateDeviceEntryIndication(!wasPluggedIn && mPowerPluggedInWired);
             }
-        }
-
-        private float getRealtimeChargingCurrent(
-                boolean isChargingOrFull, float fallbackCurrentMicroAmps) {
-            if (mBatteryManager == null || !isChargingOrFull) {
-                return fallbackCurrentMicroAmps;
-            }
-
-            final int rawCurrentMicroAmps = mBatteryManager.getIntProperty(
-                    BatteryManager.BATTERY_PROPERTY_CURRENT_NOW);
-            if (rawCurrentMicroAmps == Integer.MIN_VALUE || rawCurrentMicroAmps == 0) {
-                return fallbackCurrentMicroAmps;
-            }
-
-            float currentMicroAmps = rawCurrentMicroAmps;
-            if (currentMicroAmps < 0) {
-                currentMicroAmps = -currentMicroAmps;
-            }
-
-            if (currentMicroAmps <= 0 || currentMicroAmps > 20_000_000f) {
-                return fallbackCurrentMicroAmps;
-            }
-
-            return currentMicroAmps;
-        }
-
-        private float getRealtimeChargingVoltage(
-                Intent batteryIntent, float fallbackVoltageMicroVolts) {
-            if (batteryIntent == null) {
-                return fallbackVoltageMicroVolts;
-            }
-
-            final int voltageMilliVolts = batteryIntent.getIntExtra(
-                    BatteryManager.EXTRA_VOLTAGE, -1);
-            if (voltageMilliVolts <= 0) {
-                return fallbackVoltageMicroVolts;
-            }
-
-            final float voltageMicroVolts = voltageMilliVolts * 1000f;
-            return voltageMicroVolts >= 3_000_000f && voltageMicroVolts <= 6_000_000f
-                    ? voltageMicroVolts
-                    : fallbackVoltageMicroVolts;
-        }
-
-        private float getRealtimeChargingWattage(
-                float chargingCurrentMicroAmps, float chargingVoltageMicroVolts,
-                float fallbackWattageMicroWatts) {
-            if (chargingCurrentMicroAmps <= 0 || chargingVoltageMicroVolts <= 0) {
-                return fallbackWattageMicroWatts;
-            }
-
-            final float wattageMicroWatts =
-                    (chargingCurrentMicroAmps * chargingVoltageMicroVolts) / 1_000_000f;
-            return wattageMicroWatts > 0 && wattageMicroWatts <= 150_000_000f
-                    ? wattageMicroWatts
-                    : fallbackWattageMicroWatts;
         }
 
         @Override
