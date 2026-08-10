Lunaris-AOSP/frameworks_base

core: Introduce per-apps spoofing [1/2]
- Device models taken from PixelPropsUtils

Signed-off-by: kenway214 <kenway214@outlook.com>
Signed-off-by: Ghosuto <clash.raja10@gmail.com>

core/java/android/app/Instrumentation.java
@@ -82,6 +82,7 @@ import java.util.concurrent.TimeoutException;
 
 import com.android.internal.util.lunaris.AttestationHooks;
 import com.android.internal.util.lunaris.PixelPropsUtils;
+import com.android.internal.util.lunaris.PerAppsPropsUtils;
 
 /**
  * Base class for implementing application instrumentation code.  When running
@@ -1364,6 +1365,7 @@ public class Instrumentation {
         app.attach(context);
         AttestationHooks.setProps(context);
         PixelPropsUtils.setProps(context);
+        PerAppsPropsUtils.setProps(context);
         return app;
     }
     
@@ -1384,6 +1386,7 @@ public class Instrumentation {
         app.attach(context);
         AttestationHooks.setProps(context);
         PixelPropsUtils.setProps(context);
+        PerAppsPropsUtils.setProps(context);
         return app;
     }
 

osm1019/frameworks_base_evo

core/java/android/app/Instrumentation.java
@@ -81,6 +81,7 @@ import java.util.StringJoiner;
 import java.util.concurrent.TimeoutException;
 
 import com.android.internal.util.evolution.AttestationHooks;
+import com.android.internal.util.evolution.PerAppsPropsUtils;
 import com.android.internal.util.evolution.PixelPropsUtils;
 
 /**
@@ -1364,6 +1365,7 @@ public class Instrumentation {
         app.attach(context);
         AttestationHooks.setProps(context);
         PixelPropsUtils.setProps(context);
+        PerAppsPropsUtils.setProps(context);
         return app;
     }
     
@@ -1384,6 +1386,7 @@ public class Instrumentation {
         app.attach(context);
         AttestationHooks.setProps(context);
         PixelPropsUtils.setProps(context);
+        PerAppsPropsUtils.setProps(context);
         return app;
     }
 